8e36fc983d762bc1fbfac6d3ea94ff9a361ad0c9
Rafe-4 <rafe23105101512@diu.edu.bd>
2025-01-18 18:41:55 -0400

    operation

---
+++ operation.py
@@ -0,0 +1,34 @@
+#addition
+a = 10
+b= 30 
+print(a+b)
+
+#asunstraction
+a = 10
+b= 30 
+print(a-b)
+
+#multification
+a = 10
+b= 30 
+print(a*b)
+
+#divition
+a = 10
+b= 30 
+print(a/b)
+
+#amodulas
+a = 10
+b= 30 
+print(a%b)
+
+#exponention
+a = 10
+b= 30 
+print(a**b)
+
+#floor divition
+a = 10
+b= 30 
+print(a//b)
