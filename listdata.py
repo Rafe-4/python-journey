70d3f2a4506bc67e122e8ef20c0185968622944b
Rafe-4 <rafe23105101512@diu.edu.bd>
2025-01-20 20:59:16 -0400

    commite

--- listdata.py
+++ listdata.py
@@ -3,3 +3,16 @@ print(list)
 
 list[1]=3
 print (list)
+
+#access list item 
+list = ["rafe","safi","kafi"]
+
+print(list[1])
+
+#change list item
+
+list = ["rafe","safi","kafi"]
+list[1]= 12
+
+print(list)
+
