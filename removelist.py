a8238b75f979441a4dcc6afba671eccd1276304e
Rafe-4 <rafe23105101512@diu.edu.bd>
2025-01-20 21:31:52 -0400

    remove

---
+++ removelist.py
@@ -0,0 +1,19 @@
+#Python Remove List Item
+list = [1,2,3,4,5]
+list.remove(4)
+print(list)
+
+#pop item
+list = [1,2,3,4,5]
+list.pop(4)
+print(list)
+
+#del item
+list = [1,2,3,4,5]
+del list[1]
+print(list)
+
+#clear item
+list = [1,2,3,4,5]
+list.clear()
+print(list)
