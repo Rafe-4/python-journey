3445981d14f90aaed304508c3f4d9887baf2cd60
Rafe-4 <rafe23105101512@diu.edu.bd>
2025-01-21 23:50:56 -0400

    loop

--- looplist.py
+++ looplist.py
@@ -14,3 +14,19 @@ y=0
 while y<len(looplist):
     print(looplist[y])
     y=y+1
+
+    #list comprehension
+
+    num=[1,2,3,4,5]
+    double=[i*2 for i in num]
+    print(double)
+
+    #sort list
+    num=[1,2,91,13,3,4,5]
+    num.sort()
+    print(num)
+
+   #reverse list
+    num=[1,2,91,13,3,4,5]
+    num.sort(reverse=True)
+    print(num)  
