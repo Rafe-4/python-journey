f57ade91b02ee712339171ce3363d55d6a8b04a5
Rafe-4 <rafe23105101512@diu.edu.bd>
2025-01-21 23:26:57 -0400

    looplist

---
+++ looplist.py
@@ -0,0 +1,16 @@
+#for loop
+looplist=["rafi","kafi","safi","mafi"]
+for i in looplist:
+    print(i)
+
+#for loop position print
+looplist=["rafi","kafi","safi","mafi"]
+
+for i in range(len(looplist)):
+    print(i)
+
+#while loop
+y=0
+while y<len(looplist):
+    print(looplist[y])
+    y=y+1
